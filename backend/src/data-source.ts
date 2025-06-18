f306905ab4147386fb720f5e6b3b1fc59519d288
FreeRikato <aravinthanrc@gmail.com>
2025-06-18 17:30:45 +0530

    Fixed the issue with capital database name

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -4,21 +4,22 @@ import { DataSource } from "typeorm";
 /*
  * Here I have used a docker instance to run postgres with the command,
  *  docker run --name RemBG \
- * -e POSTGRES_USER=aravinthan \
- * -e POSTGRES_PASSWORD=postgres \
- * -e POSTGRES_DB=RemBG \
+ * -e postgres_user=aravinthan \
+ * -e postgres_password=postgres \
+ * -e postgres_db=test_rembg \
  * -p 5432:5432 \
  * -v pg_data:/var/lib/postgresql/data \
  * -d postgres
+ *
+ *  ❌ Most importantly don't use Capital letters for database name
  */
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
     port: 5432,
     username: "aravinthan",
     password: "postgres",
-    database: "RemBG",
+    database: "test_rembg",
     synchronize: true,
     logging: false,
     entities: ["src/entity/**/*.ts"], // Setting up this way i would not need to import each entities
